f84324692401fede11568ac00229dc5677c066e2
Filip Lindahl <gysztaf@gmail.com>
2016-06-18 22:34:29 +0200

    Mind is mush. Fleshed out my idea. Solution is in comments.

--- Problem3.hs
+++ Problem3.hs
@@ -7,6 +7,8 @@ What is the largest prime factor of the number 600851475143 ?
 -}
 
 -- mod [2..n-1] if any Trues, then it's not Prime.
---isPrime 1 = [False]
---isPrime 2 = [True]
-isPrime n = map (n `mod` ) [2..n-1] -- plocka ut alla nollor och kolla efter tom lista [x | x <- [], htdt]
+--isPrime 0 = False
+--isPrime 1 = False
+--isPrime 2 = True
+isPrime n = [x | x <- [map (n `mod` ) [2..n-1]], x == 0] -- Inga nollor i listan == primtal.
+--[x | x <- [0..n], (n `mod` x == 0) && (isPrime n == True)] - plocka ut primtalsfaktorer och sist i listan är störst.
