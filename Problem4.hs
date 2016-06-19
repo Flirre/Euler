36fd54d4804842fb41ade119e604efc83c40358f
Filip Lindahl <gysztaf@gmail.com>
2016-06-19 20:37:56 +0200

    Only a plan.

--- Problem4.hs
+++ Problem4.hs
@@ -1,4 +1,5 @@
 import Test.QuickCheck
+
 {-
 
 A palindromic number reads the same both ways. The largest palindrome made from the product of
@@ -17,3 +18,15 @@ isPalindrome x = show x == reverseInt x
 
 prop_reverseint :: Int -> Bool
 prop_reverseint x = show (abs x) == reverse(reverseInt (abs x))
+
+--threeDigitProducts :: [Int]
+--threeDigitProducts = [100..999] * [100..999]
+
+
+--oDPs :: [Int] -> [Int]
+--oDPs n = map (*n) [0..9]
+
+
+mul n = map (*n)
+
+times2 = map (*2) [0..10]
